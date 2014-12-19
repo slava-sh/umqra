9b7e05260c8455dc3ca432bef36d54ab7a06c53a
Slava Shklyaev <shk.slava@gmail.com>
2014-12-19 12:24:23 +0200

    Add a player and some text

--- Umqra.elm
+++ Umqra.elm
@@ -8,15 +8,41 @@ import Keyboard
 import Mouse
 import Time
 
-type alias Game = {}
+type alias Game =
+  { player : Player
+  }
+
+type alias Object a =
+  { a
+  | x : Float
+  , y : Float
+  }
+
+type alias Player = Object
+  {
+  }
 
 game : Signal Game
-game = constant {}
+game = constant
+  { player = { x = 0, y = 0 }
+  }
 
 display : (Int, Int) -> Game -> Element
-display (w, h) _ = collage w h
-  [ rect (toFloat w) (toFloat h) |> filled Color.black
-  ]
+display (w, h) {player} =
+  let halfW = toFloat w / 2
+      halfH = toFloat h / 2
+  in
+    layers
+      [ collage w h
+          [ rect (toFloat w) (toFloat h) |> filled Color.black
+          , circle 10 |> filled Color.white |> move (player.x, player.y)
+          , circle 3 |> filled Color.red |> move (0, 0)
+          ]
+      , container w h (topLeftAt (absolute 10) (absolute 10))
+          (Text.fromString "Zzz"
+           |> Text.color Color.white
+           |> Text.leftAligned)
+      ]
 
 main : Signal Element
 main = display <~ Window.dimensions ~ game
